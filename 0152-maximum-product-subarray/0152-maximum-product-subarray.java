5b2a61395d7ffc466866c6a218aa181808505e98
Nishika <112150572+Nishika-Sharma@users.noreply.github.com>
2024-07-12 10:39:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,21 @@
 class Solution {
-  public int maxProduct(int[] nums) {
-    int ans = nums[0];
-    int dpMin = nums[0];
-    int dpMax = nums[0];
+    public int maxProduct(int[] nums) {
+        
+        int max = nums[0], min = nums[0], ans = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            
+            int temp = max;  // store the max because before updating min your max will already be updated
+            
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            
+            if (max > ans) {
+                ans = max;
+            }
+        }
+        
+        return ans;
 
-    for (int i = 1; i < nums.length; ++i) {
-      final int num = nums[i];
-      final int prevMin = dpMin;
-      final int prevMax = dpMax;
-      if (num < 0) {
-        dpMin = Math.min(prevMax * num, num);
-        dpMax = Math.max(prevMin * num, num);
-      } else {
-        dpMin = Math.min(prevMin * num, num);
-        dpMax = Math.max(prevMax * num, num);
-      }
-      ans = Math.max(ans, dpMax);
     }
-    return ans;
-  }
 }
