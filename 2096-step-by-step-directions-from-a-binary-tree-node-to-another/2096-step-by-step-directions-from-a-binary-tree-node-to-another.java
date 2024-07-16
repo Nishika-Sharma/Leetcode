e4cc3c6087101624f40224a4dc5253422966c1c2
Nishika <112150572+Nishika-Sharma@users.noreply.github.com>
2024-07-16 06:59:54 +0530

    Time: 17 ms (96.06%), Space: 80.2 MB (54.8%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -0,0 +1,36 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Solution {
+    private boolean find(TreeNode n, int val, StringBuilder sb) {
+        if (n.val == val) 
+            return true;
+        if (n.left != null && find(n.left, val, sb))
+            sb.append("L");
+        else if (n.right != null && find(n.right, val, sb))
+            sb.append("R");
+        return sb.length() > 0;
+    }
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        StringBuilder s = new StringBuilder(), d = new StringBuilder();
+        find(root, startValue, s);
+        find(root, destValue, d);
+        int i = 0, max_i = Math.min(d.length(), s.length());
+        while (i < max_i && s.charAt(s.length() - i - 1) == d.charAt(d.length() - i - 1))
+            ++i;
+        return "U".repeat(s.length() - i) + d.reverse().toString().substring(i);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(4);
+        Solution solution = new Solution();
+        System.out.println(solution.getDirections(root, 3, 6));
+    }
+}
