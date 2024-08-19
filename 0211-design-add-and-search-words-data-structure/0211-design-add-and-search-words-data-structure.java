1a8cf644ec72199a24a6d0a7ba40f73da95c6145
Nishika <112150572+Nishika-Sharma@users.noreply.github.com>
2024-08-19 19:04:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,29 @@
+public class WordDictionary {
+    private List<String> words;
+
+    public WordDictionary() {
+        this.words = new ArrayList<>();
+    }
+    public void addWord(String word) {
+        this.words.add(word);
+    }
+    public boolean search(String word) {
+        for (String w : this.words) {
+            if (match(w, word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean match(String w, String word) {
+        if (w.length() != word.length()) {
+            return false;
+        }
+        for (int i = 0; i < w.length(); i++) {
+            if (word.charAt(i) != '.' && word.charAt(i) != w.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
